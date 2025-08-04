652ba5c18bb0e1f2dc750b022b7ba959c9ea0ae4
liujip0 <liujip2020@gmail.com>
2025-08-03 23:50:19 -0400

    clean up main.tsx

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,78 +2,18 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { createTheme, responsiveFontSizes, ThemeProvider } from "@mui/material";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { httpBatchLink } from "@trpc/client";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { trpc } from "./utils/trpc.ts";
-
-const theme = responsiveFontSizes(
-  createTheme({
-    palette: {
-      primary: {
-        main: "#000f5d", // Indiana Flag Blue
-        contrastText: "#fff",
-      },
-      secondary: {
-        main: "#ffb900", // Indiana Flag Gold
-        contrastText: "#000",
-      },
-      background: {
-        default: "#89cff0", // Light Indiana Flag Blue
-        paper: "#dddddd",
-      },
-    },
-    typography: {
-      h1: {
-        fontSize: "5rem",
-      },
-      h2: {
-        fontSize: "2rem",
-      },
-      body1: {
-        fontSize: "1.5em",
-      },
-      button: {
-        fontSize: "1.3em",
-      },
-    },
-    components: {
-      MuiOutlinedInput: {
-        styleOverrides: {
-          notchedOutline: {
-            borderColor: "#000f5d",
-          },
-        },
-      },
-    },
-  })
-);
-
-const queryClient = new QueryClient();
-const trpcClient = trpc.createClient({
-  links: [
-    httpBatchLink({
-      url: import.meta.env.VITE_SERVER_URL + "/api",
-      headers() {
-        return {
-          Authorization: "Bearer " + localStorage.getItem("token"),
-        };
-      },
-    }),
-  ],
-});
+import { queryClient, trpc, trpcClient } from "./utils/trpc.ts";
 
 createRoot(document.getElementById("root")!).render(
-  <ThemeProvider theme={theme}>
-    <trpc.Provider
-      client={trpcClient}
-      queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </trpc.Provider>
-  </ThemeProvider>
+  <trpc.Provider
+    client={trpcClient}
+    queryClient={queryClient}>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </trpc.Provider>
 );
--- app/src/utils/trpc.ts
+++ app/src/utils/trpc.ts
@@ -1,4 +1,19 @@
 import type { AppRouter } from "@isa2026/api/src/router.ts";
-import { createTRPCReact } from "@trpc/react-query";
+import { QueryClient } from "@tanstack/react-query";
+import { createTRPCReact, httpBatchLink } from "@trpc/react-query";
 
 export const trpc = createTRPCReact<AppRouter>();
+
+export const queryClient = new QueryClient();
+export const trpcClient = trpc.createClient({
+  links: [
+    httpBatchLink({
+      url: import.meta.env.VITE_SERVER_URL + "/api",
+      headers() {
+        return {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        };
+      },
+    }),
+  ],
+});
